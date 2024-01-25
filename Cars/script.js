e4ad9da75792c733b7e6d78b38b7e9d50eb59cf1
viksamiles77 <mileskiviktor77@gmail.com>
2024-01-25 18:01:54 +0100

    car
    code optimized

--- Cars/script.js
+++ Cars/script.js
@@ -1,39 +1,58 @@
-let carTable = document.querySelector("#car-table");
+// <=== selecting elements ===>
+const html = {
+  carTable: document.querySelector("#car-table"),
+  carTypeInput: document.querySelector("#select-car-type"),
+  carBrandInput: document.querySelector("#select-car-brand"),
+  carModelInput: document.querySelector("#car-model-input"),
+  carDoorInput: document.querySelector("#number-of-car-doors"),
+  carGasTypeInput: document.querySelector("#select-gas-type"),
+  carColorInput: document.querySelector("#select-color"),
+  resetFilterBtn: document.querySelector("#reset-filter-btn"),
+  applyFilterBtn: document.querySelector("#apply-filter-btn"),
+};
+
 const jsonURL =
   "https://raw.githubusercontent.com/viksamiles77/Viktor_JSON/main/cars.json";
 
-fetch(jsonURL)
-  .then((res) => {
-    return res.json();
-  })
-  .then((allCarsArray) => {
-    fillTheTable(allCarsArray);
-  })
-  .catch((error) => {
-    console.log("ERROR", error);
-  })
-  .finally(() => {
-    console.log("Fetch is done");
-  });
-
-function createTableCell(content) {
-  const cell = document.createElement("td");
-  cell.textContent = content;
-  return cell;
+// <=== Functions ===>
+function getCars() {
+  return fetch(jsonURL)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.log("ERROR", error);
+    })
+    .finally(() => {
+      console.log("Fetch is done");
+    });
 }
 
-function fillTheTable(allCarsArray) {
-  allCarsArray.forEach((car) => {
-    const row = document.createElement("tr");
-
-    row.appendChild(createTableCell(car.brand));
-    row.appendChild(createTableCell(car.model));
-    row.appendChild(createTableCell(car.doors));
-    row.appendChild(createTableCell(car.gasType));
-    row.appendChild(createTableCell(car.color));
-    row.appendChild(createTableCell(car.isNew ? "New" : "Used"));
-    row.appendChild(createTableCell(car.horsepower));
-
-    carTable.appendChild(row);
+function showTable(cars) {
+  cars.forEach((car) => {
+    html.carTable.innerHTML += createTable(car);
   });
 }
+
+function createTable(car) {
+  return `
+
+  <tr>
+  <td>${car.type}</td>
+  <td>${car.brand}</td>
+  <td>${car.model}</td>
+  <td>${car.doors}</td>
+  <td>${car.gasType}</td>
+  <td>${car.color}</td>
+  <td>${car.isNew ? "New" : "Used"}</td>
+  <td>${car.horsepower}</td>
+  </tr>`;
+}
+
+// <=== Events ===>
+html.applyFilterBtn.addEventListener("click", () => {
+  while (html.carTable.rows.length > 1) {
+    html.carTable.deleteRow(1);
+  }
+});
+
+//init
+getCars().then((cars) => showTable(cars));
