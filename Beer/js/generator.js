1744047536f670581ede2934102e6e522524f08b
viksamiles77 <mileskiviktor77@gmail.com>
2024-02-08 01:20:52 +0100

    beer update

--- Beer/js/generator.js
+++ Beer/js/generator.js
@@ -1,14 +1,14 @@
-const randomBeerUrl = "https://api.punkapi.com/v2/beers/random";
-
 // selecting elements
 const imgWrap = document.querySelector("#img-wrapper");
 const generateBtn = document.querySelector("#generate-btn");
 const viewMenuBtn = document.querySelector("#menu-btn");
 const beerNameText = document.querySelector("#beer-name");
 const beerDescriptionText = document.querySelector("#beer-description");
+const randomBeerUrl = "https://api.punkapi.com/v2/beers/random";
 
 // functions
 async function fetchBeer() {
+  // fetch the beers
   const response = await fetch(`${randomBeerUrl}`);
   const data = await response.json();
   return data;
@@ -54,6 +54,7 @@ function displayRandomBeer() {
 // init
 displayRandomBeer();
 
+// event listeners
 generateBtn.addEventListener("click", displayRandomBeer);
 viewMenuBtn.addEventListener("click", () => {
   window.location.href = "menu.html";
--- Beer/js/menu.js
+++ Beer/js/menu.js
@@ -1,21 +1,93 @@
 // selecting elements
-
-let currentPage = 1;
 const searchBeer = document.querySelector("#beer-search");
 const selectPagination = document.querySelector("#select-pagination");
+const resultContainer = document.querySelector("#result");
+const checkbox = document.querySelector("#checkbox");
 const backToGenBtn = document.querySelector("#back-to-generator-btn");
-const resultDiv = document.querySelector("#result");
+const nextPageBtn = document.querySelector("#next-button");
+const previousPageBtn = document.querySelector("#previous-button");
+let currentPage = 1;
+let perPage = 25;
 
 // functions
-
 async function getBeers() {
   try {
-    const response = await fetch(
-      `https://api.punkapi.com/v2/beers?page=2&per_page=80`
-    );
+    const searchValue = searchBeer.value.trim();
+    const paginationValue = selectPagination.value;
+    let apiUrl = `https://api.punkapi.com/v2/beers?page=${currentPage}&per_page=${perPage}`;
+
+    if (searchValue) {
+      apiUrl += `&beer_name=${searchValue}`;
+    }
+
+    if (checkbox.checked) {
+      apiUrl += `&abv_gt=6`;
+      console.log("its checked");
+    }
+
+    const response = await fetch(apiUrl);
     const beers = await response.json();
     return beers;
   } catch (error) {
     console.log(error);
   }
 }
+
+function createTable(beers) {
+  let table = `
+        <table class="beer-table">
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Food Pairing</th>
+            <th>ABV</th>
+            <th>EBC</th>
+            <th>IBU</th>
+          </tr>
+      `;
+
+  beers.forEach((beer) => {
+    table += `
+          <tr>
+            <td>${beer.id}</td>
+            <td>${beer.name}</td>
+            <td>${beer.food_pairing}</td>
+            <td>${beer.abv}</td>
+            <td>${beer.ebc}</td>
+            <td>${beer.ibu}</td>
+          </tr>
+        `;
+  });
+  table += `</table>`;
+
+  return table;
+}
+
+async function showBeers() {
+  const beers = await getBeers();
+  const table = createTable(beers);
+  resultContainer.innerHTML = table;
+}
+
+// event listeners
+searchBeer.addEventListener("input", () => {
+  currentPage = 1;
+  showBeers();
+});
+
+backToGenBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+
+nextPageBtn.addEventListener("click", () => {
+  currentPage++;
+  showBeers();
+});
+
+previousPageBtn.addEventListener("click", () => {
+  currentPage--;
+  showBeers();
+});
+
+// init
+showBeers();
